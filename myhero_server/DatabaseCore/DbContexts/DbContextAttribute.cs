4fa54c38f27e902341e5827aa349a67a60804c0d
Lovecookie <silver2000cs@gmail.com>
2024-01-17 18:43:26 +0900

    gitignore 추가

---
+++ myhero_server/DatabaseCore/DbContexts/DbContextAttribute.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace myhero_dotnet.DatabaseCore.DbContexts;
+
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+public class DbSchemaAttribute : System.Attribute
+{
+	public string DbName { get; init; }
+
+	public string Schema { get; init; }
+
+	public DbSchemaAttribute(string name, string schema)
+	{
+		DbName = name;
+		Schema = schema;
+	}
+}
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+public class DbTableAttribute : System.Attribute
+{ 
+	public string TableName { get; init; }
+
+	public DbTableAttribute(string tableName)
+	{
+		TableName = tableName;
+	}
+}
